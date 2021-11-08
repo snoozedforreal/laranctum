be02e4100f1466112b82486173c651a280dd68b1
Hassan Ait Nacer <itshassanaitnacer@gmail.com>
2021-11-08 18:13:17 +0100

    Fields lang lines added

---
+++ resources/lang/en/fields.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fields Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to specify fields names.
+    |
+    */
+
+    'email' => 'email',
+    'password' => 'password',
+
+];
