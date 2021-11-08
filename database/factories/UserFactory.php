38f8dfec51d4cbabf07517b7322c5a82bbc8b178
Hassan Ait Nacer <itshassanaitnacer@gmail.com>
2021-11-08 18:49:54 +0100

    Username field added to user model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,6 +16,7 @@ class UserFactory extends Factory
         return [
             'first_name' => $this->faker->firstName(),
             'last_name' => $this->faker->lastName(),
+            'username' => $this->faker->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
