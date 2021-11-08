29c0a5ac443b6a6a5f6b0374536d909a6bb0291b
Hassan Ait Nacer <itshassanaitnacer@gmail.com>
2021-11-08 18:21:33 +0100

    Split name to first and last name in users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,8 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
